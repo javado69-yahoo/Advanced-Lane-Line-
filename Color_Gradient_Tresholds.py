f40c2aea36dac427ed95eadac535c4d5083764b2
javado69-yahoo <103825664+javado69-yahoo@users.noreply.github.com>
2023-02-19 23:24:07 +0330

    Update Color_Gradient_Tresholds.py

--- Color_Gradient_Tresholds.py
+++ Color_Gradient_Tresholds.py
@@ -7,27 +7,7 @@ import matplotlib.image as mpimg
 
 #absolute value and applies a threshold.
 
-
-
-def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
-    # 1) Convert to grayscale
-    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    # 2) Take the derivative in x or y given orient = 'x' or 'y'
-    # 3) Take the absolute value of the derivative or gradient
-    if orient=='x':
-        abs_sobel=np.abs(cv2.Sobel(gray,cv2.CV_64F,1,0))
-    if orient=='y':
-        abs_sobel=np.abs(cv2.Sobel(gray,cv2.CV_64F,0,1))
-    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
-    scaled_soble=np.uint8(255*abs_sobel/np.max(abs_sobel))
-    # 5) Create a mask of 1's where the scaled gradient magnitude 
-            # is > thresh_min and < thresh_max
-    binary_output=np.zeros_like(scaled_soble)
-    binary_output[(scaled_soble>=thresh_min) & (scaled_soble<=thresh_max)]=1
-    # 6) Return this mask as your binary_output image
-    return binary_output
     
-'''STEP_2'''
 def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -62,3 +42,30 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     # Return the binary image
     return binary_output
 
+def color_thresh(img, r_thresh=(0, 255), s_thresh=(0, 255)):
+    """
+    Returns a binary image of the same size as the input image of ones where pixel values
+    were in the threshold range, and zeros everywhere else.
+    :param img: input image in BGR format.
+    :param r_thresh: threshold (0 to 255) for determining which pixels from r_channel to include in binary output.
+    :param s_thresh: threshold (0 to 255) for determining which pixels from s_channel to include in binary output.
+    """
+    # Apply a threshold to the R channel
+    r_channel = img[:,:,2]
+    r_binary = np.zeros_like(img[:,:,0])
+    # Create a mask of 1's where pixel value is within the given thresholds
+    r_binary[(r_channel > r_thresh[0]) & (r_channel <= r_thresh[1])] = 1
+
+    # Convert to HLS color space
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    # Apply a threshold to the S channel
+    s_channel = hls[:,:,2]
+    s_binary = np.zeros_like(s_channel)
+    # Create a mask of 1's where pixel value is within the given thresholds
+    s_binary[(s_channel > s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
+    # Combine two channels
+    combined = np.zeros_like(img[:,:,0])
+    combined[(s_binary == 1) | (r_binary == 1)] = 1
+    # Return binary output image
+    return combined
